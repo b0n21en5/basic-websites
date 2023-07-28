382b977c789bd6ea8cb18f9f492452ffe76e4c24
Bikash Nath <72164894+b0n21en5@users.noreply.github.com>
2023-07-28 18:28:43 +0530

    Drum Kit Website - HTML, CSS, JS

--- Drum-Kit/script.js
+++ Drum-Kit/script.js
@@ -1,34 +1,54 @@
-const drum1 = document.getElementById("drum-1");
-drum1.addEventListener("click", () => {
-  let audio = new Audio("assets/sounds/d1.wav");
-  audio.play();
+const allDrums = document.querySelectorAll(".drums");
+
+// Added event listener on all drums
+allDrums.forEach((drum) => {
+  drum.addEventListener("click", () => {
+    const drumInnerHtml = drum.innerHTML;
+
+    playDrumAudio(drumInnerHtml);
+    applyAnimationDrum(drumInnerHtml);
+  });
 });
 
-const drum2 = document.getElementById("drum-2");
-drum2.addEventListener("click", () => {
-  let audio = new Audio("assets/sounds/d2.wav");
-  audio.play();
+// Added event listener for keyboard
+document.addEventListener("keypress", (event) => {
+  playDrumAudio(event.key);
+  applyAnimationDrum(event.key);
 });
 
-const drum3 = document.getElementById("drum-3");
-drum3.addEventListener("click", () => {
-  let audio = new Audio("assets/sounds/d3.wav");
-  audio.play();
-});
+// Function to handle playing audio
+function playDrumAudio(key) {
+  switch (key) {
+    case "a":
+      let drum1 = new Audio("assets/sounds/d1.wav");
+      drum1.play();
+      break;
+    case "w":
+      let drum2 = new Audio("assets/sounds/d2.wav");
+      drum2.play();
+      break;
+    case "s":
+      let drum3 = new Audio("assets/sounds/d3.wav");
+      drum3.play();
+      break;
+    case "d":
+      let drum4 = new Audio("assets/sounds/d4.wav");
+      drum4.play();
+      break;
+    case "f":
+      let drum5 = new Audio("assets/sounds/d5.mp3");
+      drum5.play();
+      break;
+  }
+}
 
-const drum4 = document.getElementById("drum-4");
-drum4.addEventListener("click", () => {
-  let audio = new Audio("assets/sounds/d4.wav");
-  audio.play();
-});
+// Apply animation to clicked drum
+function applyAnimationDrum(currKey) {
+  const activeButton = document.querySelector("#" + currKey);
+  console.log(activeButton);
+  activeButton.classList.add("pressed");
 
-const drum5 = document.getElementById("drum-5");
-drum5.addEventListener("click", () => {
-  let audio = new Audio("assets/sounds/d5.mp3");
-  audio.play();
-});
-
-document.addEventListener("keydown", () => {
-  let audio = new Audio("assets/sounds/d1.wav");
-  audio.play();
-});
+  setTimeout(() => {
+    activeButton.classList.remove("pressed");
+  }, 800);
+}
